ffc347bd22efc9bcac21c24464c24cb05f55fe80
Ewsgit <ewsgit@duck.com>
2023-03-01 13:19:39 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         appDir: true,
     },
-    reactStrictMode: false
+    reactStrictMode: false,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
 }
 
 module.exports = nextConfig
